c3cd2ebba0cb5b021764211e426b30d11122f21c
Cat Colbert <c.perkins.e@gmail.com>
2026-03-18 07:19:07 -0400

    chore: update ESLint and TypeScript dependencies, configure ESLint to ignore coverage directory

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -4,11 +4,15 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig(
+  { ignores: ['dist', 'coverage'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
